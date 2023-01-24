38bf5b48ed135f8219aa901f8d317441064c87af
Cyber Anubis <107937836+x86-Anubis@users.noreply.github.com>
2023-01-24 15:41:43 +0200

    Create TemplateClass.cpp

---
+++ C-C++ Essentials/TemplateClass.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+template<class T>
+class  Arithametic{
+private:
+    T a,b;
+public:
+    Arithametic(T a,T b);
+        T add();
+        T sub();
+
+};
+template<class T>
+Arithametic<T>::Arithametic(T a, T b) {
+    this->a=a;
+    this->b=b;
+}
+template<class T>
+T Arithametic<T>::add(){
+    T c = a+b;
+    return c;
+}
+template<class T>
+T Arithametic<T>::sub() {
+    T c = a-b;
+    return c;
+}
+
+int main()
+{
+ Arithametic ar(15,10);
+ cout<<"int : "<<ar.add()<<", int sub: "<<ar.sub()<<endl;
+ Arithametic arF(15.2,12.4);
+ cout<<"Float : "<<arF.add()<<", Float sub: "<<arF.sub()<<endl;
+
+}
