8dbfcd7068303404758070cfaa526877df2530df
Cyber Anubis <107937836+x86-Anubis@users.noreply.github.com>
2023-01-26 19:29:56 +0200

    Update Print from 1 to N.cpp

--- Recursion/Print from 1 to N.cpp
+++ Recursion/Print from 1 to N.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
 
-void pr(int i,int c){
-    if(i!=0){
-        cout<<c-i<<endl;
-        pr(i-1,c);
-    }
+int rec(int i){
+if(i>0){
+    rec(i-1);
+    cout<<i<<endl;
+}
+
+  return 0;
 }
 int main() {
-// printing numbers from 1 -> n with recursion
 int n;cin>>n;
-int c = n+1;
-pr(n,c);
+rec(n);
+
 }
