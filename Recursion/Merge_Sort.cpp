2e273d86fe574858a75d1fe93b79a9986470a47b
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2023-08-23 13:38:19 +0300

    Create Merge_Sort.cpp

---
+++ Recursion/Merge_Sort.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <list>
+#include <queue>
+#include <unordered_set>
+#include <utility>
+#include <string>
+//#include <boost/multiprecision/cpp_int.hpp>
+#define int64 long long
+#define all(v) ((v).begin()), ((v).end())
+#define sz(v) ((int)(v).size())
+#define ON(n , k) ((n) | (1<<k))
+#define uint unsigned long long
+#define pb push_back
+#define OFF(n , k) ((n) & ~(1<<k))
+#define isON(n , k) (((n)>>(k))&1)
+#define FLIP(n , k) ((n)^(1<<k))
+#define int long long
+#define output(v) for(auto&it:v){cout<<it<<" ";}cout<<"\n"
+#define input(v) for(auto&it:v){cin>>it;}
+#define ll long long
+#define endl '\n'
+#define ordered_set tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>
+using namespace std;
+void NARUTO_UZUMAKI(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+void UCHIHA_SASUKI(void) {
+#ifndef ONLINE_JUDGE
+    freopen("/home/ebrahimelsayed/Desktop/NEW/input.txt", "r", stdin);
+    //  freopen("output.txt", "w", stdout);
+
+#endif
+}
+void preCalc()
+{
+
+}
+
+void merge(vector<int>&v , int s , int e){
+    int i = s ;
+    int mid = s + (e - s)/2;
+    int j = mid + 1;
+    vector<int>tmp;
+    while(i <= mid and j <= e){
+        if(v[i] < v[j])
+            tmp.pb(v[i++]);
+        else
+            tmp.pb(v[j++]);
+    }
+    while(i <= mid)
+        tmp.pb(v[i++]);
+    while(j <= e)
+        tmp.pb(v[j++]);
+    int cnt = 0 ;
+    for(int l = s ; l <= e ; l++)
+        v[l] = tmp[cnt++];
+}
+
+void mergesort(vector<int>&v , int s , int e){
+    if(s >= e) return;
+    int mid = s + (e - s)/2;
+    mergesort(v, s , mid);
+    mergesort(v , mid + 1 , e);
+    return merge(v , s , e);
+}
+void HANMA_BAKI(){
+vector<int>v =  { 17,2,14,2,1,4,0};
+int s = 0 ;
+int e = sz(v) - 1;
+    mergesort(v , s , e);
+    for(int i = 0 ; i < sz(v) ; i++){
+        cout << v[i] << " ";
+    }
+}
+
+signed main()
+{
+    NARUTO_UZUMAKI();
+    UCHIHA_SASUKI();
+    // freopen("window.in", "r", stdin);
+    preCalc();
+    int t = 1 ;
+    //cin >> t;
+
+
+    while(t--)
+        HANMA_BAKI();
+
+}
